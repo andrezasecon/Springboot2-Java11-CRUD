096807778dbdac1b373c956dba9a2e1e227ed49b
Andreza Secon <andrezasecon@gmail.com>
2021-04-13 15:05:30 -0300

    Project created

---
+++ src/test/java/com/andrezasecon/crud/SpringbootCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.andrezasecon.crud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
