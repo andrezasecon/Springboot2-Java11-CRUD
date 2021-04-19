4f3821bb21a83159ee1cca2ddd48f51a215786aa
Andreza Secon <andrezasecon@gmail.com>
2021-04-19 17:02:08 -0300

    Product-OrderItem one-to-many association

--- src/main/java/com/andrezasecon/crud/entities/Product.java
+++ src/main/java/com/andrezasecon/crud/entities/Product.java
@@ -11,7 +11,10 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 @Entity
 @Table(name = "tb_product")
@@ -32,6 +35,9 @@ public class Product implements Serializable {
 	joinColumns = @JoinColumn(name = "product_id"), // nome do campo de id do produto na tabela de relacionamento
 	inverseJoinColumns = @JoinColumn(name = "category_id")) // // nome do campo de id da categoria na tabela de relacionamento	
 	private Set<Category> categories = new HashSet<>();
+	
+	@OneToMany(mappedBy = "id.product")
+	private Set<OrderItem> items = new HashSet<>();
 	
 	public Product() {
 		
@@ -88,6 +94,15 @@ public class Product implements Serializable {
 
 	public Set<Category> getCategories() {
 		return categories;
+	}
+	
+	@JsonIgnore
+	public Set<Order> getOrders(){
+		Set<Order> set = new HashSet<>();
+		for(OrderItem x : items) {
+			set.add(x.getOrder());
+		}
+		return set;
 	}
 
 	@Override
